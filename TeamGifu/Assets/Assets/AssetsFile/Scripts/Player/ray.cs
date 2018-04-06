47dcbb948a7f5d550ff5e5026f2bdf214fbad428
Ka2019 <kkam0707@gmail.com>
2018-04-06 11:33:50 +0900

    ray

--- TeamGifu/Assets/Assets/AssetsFile/Scripts/Player/ray.cs
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Player/ray.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using  UnityStandardAssets.Characters.FirstPerson;
-public class A_doubt : MonoBehaviour {
 
-
-    // Update is called once per frame
-    void Update () {
-        //Rayの作成　　　　　　　↓Rayを飛ばす原点　　　↓Rayを飛ばす方向
-        Ray ray = new Ray(transform.position, new Vector3(1, 0, 0));
+public class ray : MonoBehaviour {
+    public static bool flag;
+	// Use this for initialization
+	void Start () {
+        flag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(transform.position, new Vector3(0, 0, 1));
 
         //Rayが当たったオブジェクトの情報を入れる箱
         RaycastHit hit;
 
         //Rayの飛ばせる距離
-
+        int distance = 10;
 
         //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの     
         if (Physics.Raycast(ray, out hit))
         {
             //Rayが当たったオブジェクトのtagがPlayerだったら
             if (hit.collider.tag == "cube")
+            {
                 Debug.Log("Rayがcubeに当たった");
+                flag = true;
+            }
+   
+
         }
-        //
+        else
+        {
+            flag = false;
+        }
     }
 }
-
