02d7ef970fd5c3a983911ee66f2b43886d00bfed
motoya <hubuki008@gmail.com>
2018-04-24 15:54:43 +0900

    アイテム一覧途中(1)

--- TeamGifu/Assets/Assets/AssetsFile/Scripts/Item/BagController.cs
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Item/BagController.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BagController : MonoBehaviour {
 
     private bool openBag = false;//開いている(true)いない(false)
+    static Canvas bag;
+    public static bool LookFlag=true;
+    public bool LookFlagState
+    {
+        get
+        {
+            return LookFlag;
+        }
+        set
+        {
+            LookFlag = value;
+        }
+    }
+
     public void Event()
     {
         if (openBag == false)
@@ -12,12 +27,15 @@ public class BagController : MonoBehaviour {
             Bag.SetActive("ItemList", true);
 
             openBag = true;
+            LookFlag = false;
         }
         else
         {
             Bag.SetActive("ItemList", false);
 
             openBag = false;
+            LookFlag = true;
+            
         }
 
     }
