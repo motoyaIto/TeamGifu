1b05f07a5480731478186f4e2a21ae46e3a950db
HiromuHayasi <galaxy68xxx@gmail.com>
2018-05-21 15:03:22 +0900

    [マテリアル]修正

---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Room2/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject obj;
+    public Material mtl;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+    public void Action()
+    {
+        Debug.Log("Change!!");
+        obj.GetComponent<Renderer>().material = mtl;
+    }
+}
