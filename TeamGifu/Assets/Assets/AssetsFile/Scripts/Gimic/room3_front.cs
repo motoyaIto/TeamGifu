423f77953d8631333f760672582b18b7349894d3
Ka2019 <kkam0707@gmail.com>
2018-05-24 12:29:19 +0900

    [ギミックの完成]重さギミックの出来上がり

---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/room3_front.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room3_front : MonoBehaviour {
+
+    private bool LockFlag = false;
+    public bool LockFlagState
+    {
+        set
+        {
+            LockFlag = value;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Collider>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LockFlag)
+        {
+            GetComponent<Collider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/room3_left.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room3_left : MonoBehaviour {
+    private bool LockFlag=false;
+    public bool LockFlagState
+    {
+        set
+        {
+            LockFlag = value;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        GetComponent<Collider>().enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(LockFlag)
+        {
+            GetComponent<Collider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
