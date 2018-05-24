81eba3fe4c264a80bfd157f50cb6fcd0c3ca426c
HiromuHayasi <galaxy68xxx@gmail.com>
2018-05-24 11:33:36 +0900

    [調整]自動ドアの挙動の調整＆Hierarchyの整理

--- TeamGifu/Assets/Assets/AssetsFile/Scripts/Room2/Door_Open.cs
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Room2/Door_Open.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Door_Open : MonoBehaviour {
+    #region
+    [HideInInspector]
+    public bool open_Flag;  // ドアを開けるためのフラグ
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private Material mtl;   // 変更するマテリアル
+    [SerializeField]
+    private GameObject obj;   // 変更するObject
+
+    private bool Lock_State;    // 今のロック状態
+
+    #endregion
+
+    private void Awake()
+    {
+        //mtl = GetComponent<Renderer>().material;
+        open_Flag = false;
+        Lock_State = true;
+    }
+
+    public void IsTrue()
+    {
+        if (Lock_State)
+        {
+            UnLock();
+        }
+        
+    }
+
+    // 鍵の解錠
+    private void UnLock()
+    {
+        ReplaceMaterial(mtl);
+        open_Flag = true;
+    }
+
+    // マテリアルの変更
+    private void ReplaceMaterial(Material mat)
+    {
+        obj.GetComponent<Renderer>().material = mat;
+    }
 }
