c62bcbe6b5330b7e55a06cf16d2af04a382aabdf
HiromuHayasi <galaxy68xxx@gmail.com>
2018-05-28 12:25:48 +0900

    [ギミックの調整]

---
+++ TeamGifu/Assets/Assets/Hiromu/Open.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour {
+    public GameObject obj;
+
+    public void Clear()
+    {
+        obj.SetActive(false);
+    }
+}
