bc3a3004f7f696511714653b63ab3dfb953e83fe
HiromuHayasi <galaxy68xxx@gmail.com>
2018-05-31 18:18:49 +0900

    [Lightギミック]完成

---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/Room2/LightGimick3.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightGimick3 : MonoBehaviour {
+    //プレファブ生成するアイテム
+    private GameObject PrefabItem;
+    [SerializeField, Header("ItemListControllerのオブジェ")]
+    private ItemListController _item;
+    [SerializeField, Header("固定オブジェの座標")]
+    private Vector3 ObjPos;
+    [SerializeField, Header("Rayのオブジェクト")]
+    private ray _ray;
+    private bool flag;
+
+    [HideInInspector]
+    public bool clear_Flag;
+    [Header("正解ランプのマテリアル")]
+    public Material mtl;   // 変更するマテリアル
+    [Header("不正解ランプのマテリアル")]
+    public Material mtlR;   // 変更するマテリアル
+
+    [Header("クリア判定を示すオブジェクト")]
+    public GameObject obj;
+
+    private GameObject delete_Obj;
+
+
+    private const string CreateName = "Battery_small_06 (2)";//固定オブジェクトに出す名前
+    private bool OnSwitch = false;
+    // Use this for initialization
+    void Start()
+    {
+        flag = false;
+        clear_Flag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //生成するオブジェクトを取得
+        PrefabItem = (GameObject)Resources.Load("Prefabs/" + _item.GetSelectImage());
+
+        if (!flag)
+        {
+            //ゲームオブジェクトの名前とRayに当たっているオブジェクト名が一緒か＆＆固定オブジェクトに当たっているか＆＆出すオブジェクトと選択しているアイテムの名前が一緒か
+            if (gameObject.name == _ray.RayHitNameState && _ray.LockObjFlagState && CreateName == _item.GetSelectImage())
+            {
+                GameObject obj = Instantiate(PrefabItem, new Vector3(ObjPos.x, ObjPos.y, ObjPos.z), transform.rotation);
+
+                _ray.LockObjFlagState = false;
+
+                Clear();
+            }
+            else if (gameObject.name == _ray.RayHitNameState && _ray.LockObjFlagState)
+            {
+
+                //生成するオブジェクトを取得
+                GameObject DummyPrefabItem = (GameObject)Resources.Load("Prefabs/" + _item.GetSelectImage());
+                GameObject obj = Instantiate(DummyPrefabItem, new Vector3(ObjPos.x, ObjPos.y, ObjPos.z), transform.rotation);
+
+            }
+
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("HitCapsel");
+        if (collision.gameObject.tag == "Item")
+        {
+            flag = true;
+        }
+        delete_Obj = collision.gameObject;
+    }
+
+    // クリア処理
+    private void Clear()
+    {
+        clear_Flag = true;
+        ReplaceMaterial(mtl);
+    }
+
+    public bool GetOnSwitch()
+    {
+        return OnSwitch;
+    }
+
+    // マテリアルの変更
+    private void ReplaceMaterial(Material mat)
+    {
+        obj.GetComponent<Renderer>().material = mat;
+    }
+
+    public void Delete()
+    {
+        flag = false;
+        Destroy(delete_Obj);
+        ReplaceMaterial(mtlR);
+    }
+}
