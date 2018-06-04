80a148cfc1c38f622f4938a746c671f05148d5ac
motoya <hubuki008@gmail.com>
2018-06-04 17:36:54 +0900

    [ハノイの塔]配列制御制作途中

--- TeamGifu/Assets/Assets/AssetsFile/Scripts/Hanoinotou/HanoinotouController.cs
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Hanoinotou/HanoinotouController.cs
@@ -7,7 +7,7 @@ public class HanoinotouController : MonoBehaviour {
     private static int PILLAR_NAM = 3;//柱の合計
     private static int RING_NAM = 3;//リングの合計
 
-    
+
     [SerializeField]
     private Camera PlayerCamera;//プレイヤーカメラ
     [SerializeField]
@@ -24,7 +24,16 @@ public class HanoinotouController : MonoBehaviour {
     private GameObject[] Ring = new GameObject[RING_NAM];//輪1~3
 
     private GameObject[][] stack = new GameObject[PILLAR_NAM][];//各柱の輪を管理する
-	// Use this for initialization
+
+    private struct Token
+    {
+        public GameObject obj;
+        public int start_pillar;
+    };
+
+    Token token;
+
+    // Use this for initialization
 	void Start () {
         for(int i = 0; i < PILLAR_NAM; i++)
         {
@@ -41,6 +50,7 @@ public class HanoinotouController : MonoBehaviour {
         //あたり判定
         HanoinotouColliders = GetComponentsInChildren<Collider>();
 
+        //各柱のあたり判定を消す
         foreach (Collider col in HanoinotouColliders)
         {
             if ("Pillar1" == col.name || "Pillar2" == col.name || "Pillar3" == col.name)
@@ -49,15 +59,10 @@ public class HanoinotouController : MonoBehaviour {
             }
         }
 
-        for(int i = 0; i < PILLAR_NAM; i++)
+        //初期位置にすべてスタックする
+        for(int i = RING_NAM - 1; i >= 0; i--)
         {
-            for (int j = 0; j < RING_NAM; j++)
-            {
-                if (i == 0)
-                {
-                    stack[i][j] = Ring[j];
-                }
-            }
+            stack[0][i] = Ring[i];
         }
     }
 	
@@ -66,8 +71,41 @@ public class HanoinotouController : MonoBehaviour {
 		if(_GameSyste.GetHanoinoTouFlag())
         {
             SwitchHanoinotou(false, true);
-            
-            
+
+            //各柱のあたり判定
+            //foreach (Collider col in HanoinotouColliders)
+            //{
+            //   switch(col.name)
+            //    {
+            //        case "Pillar1":
+            //            //token.obj = PushBuck(stack[0]);
+
+            //            //Vector3 pos = new Vector3(token.obj.transform.position.x, token.obj.transform.position.y +1.0f, token.obj.transform.position.z);
+            //            //token.obj.transform.position = pos;
+            //            if (Input.GetMouseButtonDown(0))
+            //            {
+            //                Debug.Log("Pillar1");
+            //            }
+            //            break;
+
+            //        case "Pillar2":
+            //            PushBuck(stack[1]);
+            //            if (Input.GetMouseButtonDown(0))
+            //            {
+            //                Debug.Log("Pillar2");
+            //            }
+            //            break;
+
+            //        case "Pillar3":
+            //            PushBuck(stack[2]);
+            //            if (Input.GetMouseButtonDown(0))
+            //            {
+            //                Debug.Log("Pillar3");
+            //            }
+            //            break;
+            //    }
+              
+            //}
         }
 	}
 
@@ -83,11 +121,51 @@ public class HanoinotouController : MonoBehaviour {
            
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        //各柱のあたり判定
+        foreach (Collider col in HanoinotouColliders)
+        {
+            switch (col.name)
+            {
+                case "Pillar1":
+                    //token.obj = PushBuck(stack[0]);
+
+                    //Vector3 pos = new Vector3(token.obj.transform.position.x, token.obj.transform.position.y +1.0f, token.obj.transform.position.z);
+                    //token.obj.transform.position = pos;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Debug.Log("Pillar1");
+                    }
+                    break;
+
+                case "Pillar2":
+                    PushBuck(stack[1]);
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Debug.Log("Pillar2");
+                    }
+                    break;
+
+                case "Pillar3":
+                    PushBuck(stack[2]);
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Debug.Log("Pillar3");
+                    }
+                    break;
+            }
+        }
 
 
+    }
 
-
-
+    /// <summary>
+    /// あたり判定を変える
+    /// </summary>
+    /// <param name="hanoinotou">onにする(true)offにする(false)</param>
+    /// <param name="pillar">onにする(true)offにする(false)</param>
     private void SwitchHanoinotou(bool hanoinotou, bool pillar)
     {
         foreach (Collider col in HanoinotouColliders)
@@ -102,5 +180,23 @@ public class HanoinotouController : MonoBehaviour {
                 col.enabled = pillar;
             }
         }
+    }
+
+    /// <summary>
+    /// 最後尾のオブジェクトを参照
+    /// </summary>
+    /// <param name="obj">オブジェクト配列</param>
+    /// <returns>最後尾のオブジェクト</returns>
+    private GameObject PushBuck(GameObject[] obj)
+    {
+        for (int i = PILLAR_NAM - 1; i >= 0; i--)
+        {
+            if(obj[i] != null)
+            {
+                return Ring[i];
+            }
+        }
+
+        return null;
     }
 }
