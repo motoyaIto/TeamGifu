79b979081c4820cc9e52600e5359716280ec2655
HiromuHayasi <galaxy68xxx@gmail.com>
2018-06-07 10:54:44 +0900

    【ギミック調整】

---
+++ TeamGifu/Assets/Assets/Hiromu/testscale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 0.1f);
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
+}
