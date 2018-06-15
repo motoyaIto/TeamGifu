03400c12967db1fab15c9ea64bb9accad2ae515a
motoya <hubuki008@gmail.com>
2018-06-15 11:37:07 +0900

    [シーンマネージャー]完成

---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/SceneManagerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManagerController : MonoBehaviour
+{
+    private AsyncOperation ope = null;//シーンを格納
+
+    //各シーン名
+    public enum SceneName
+    {
+        TITLE,
+        SCENEMANAGER,
+
+        MAX
+
+    };
+
+	// Use this for initialization
+	void Start ()
+    {
+        //次のシーンを読み込む
+        if (SceneManager.GetActiveScene().buildIndex < (int)SceneName.MAX)
+        {
+            ope = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            ope = SceneManager.LoadSceneAsync(0);
+        }
+
+        //自動再生を無効にする
+        ope.allowSceneActivation = false;
+    }
+
+    /// <summary>
+    /// シーン切替
+    /// </summary>
+    public void ChangeCene()
+    {
+        //再生する
+        ope.allowSceneActivation = true;
+        
+    }
+
+
+}
