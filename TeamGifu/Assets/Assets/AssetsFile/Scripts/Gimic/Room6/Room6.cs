309af21c771182e8cd3c012f0527ff7c1c06831f
Ka2019 <kkam0707@gmail.com>
2018-06-20 09:24:05 +0900

    Room6途中

--- TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/Room6/Room6.cs
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/Room6/Room6.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using DG.Tweening;
 
 public class Room6 : MonoBehaviour {
+    private const int MAX_LIGHT = 3;
     [SerializeField]
     Room6_UpFloor _upFloor;
     [SerializeField]
@@ -14,7 +15,7 @@ public class Room6 : MonoBehaviour {
     private Room6_LeftLight _leftLight;
     [SerializeField]
     private Room6_RightLight _rightLight;
-    private const int MAX_LIGHT = 3;
+
     [SerializeField]
     private Room6_R_SwithR r_SwithR;
     [SerializeField]
@@ -30,6 +31,29 @@ public class Room6 : MonoBehaviour {
     private Vector3 LeftPos;
     [Header("正解ランプのマテリアル")]
     public Material mtl;   // 変更するマテリアル
+    [SerializeField]
+    private Room6_L_SwithR l_swithR;
+    [SerializeField]
+    private Room6_L_SwithR l_swithL;
+    [SerializeField]
+    private L_UpFloor l_upFoor;
+    [SerializeField]
+    private GameObject L_RightFloor;
+    [SerializeField]
+    private Vector3 L_RightPos;
+
+    [SerializeField]
+    private GameObject L_LeftFloor;
+    [SerializeField]
+    private Vector3 L_LeftPos;
+    [SerializeField]
+    Room6L_FrontLight L_frontLight;
+    [SerializeField]
+    Room6L_LeftLight L_leftLight;
+    [SerializeField]
+    Room6L_RightLight L_rightLight;
+    [SerializeField]
+    private GameObject[] L_Lightobj;
 
 
     // Use this for initialization
@@ -48,6 +72,13 @@ public class Room6 : MonoBehaviour {
             _upFloor.GetRideFlag(true);
 
         }
+        if(l_swithL.clearFlagState&&l_swithR.clearFlagState)
+        {
+            L_RightFloor.transform.DOMove( L_RightPos, 2.0f);
+            L_LeftFloor.transform.DOMove(L_LeftPos, 2.0f);
+            l_upFoor.GetRideFlag(true);
+
+        }
         if(_flontLight.ClearFlagState&&_leftLight.ClearFlagState&&_rightLight.ClearFlagState)
         {
 
@@ -57,6 +88,14 @@ public class Room6 : MonoBehaviour {
             }
      
         }
+        if(L_frontLight.ClearFlagState&&L_rightLight.ClearFlagState&&L_leftLight.ClearFlagState)
+        {
+            for (int i = 0; i < MAX_LIGHT; i++)
+            {
+                L_Lightobj[i].GetComponent<Renderer>().material = mtl;
+            }
+        }
+
 
 
     }
