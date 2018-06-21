7fa5b0b9fb58be27340da93f844804e9a619a4d6
Ka2019 <kkam0707@gmail.com>
2018-06-21 18:13:49 +0900

    [ファイルの追加]火のあぶり出し

---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/Room5/FadaText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FadaText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<TextMeshProUGUI>().alpha = 0.0f;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hit")
+        {
+            Debug.Log("hit");
+            fadeFunction();
+
+        }
+    }
+    public void fadeFunction()
+    {
+        this.GetComponent<TextMeshProUGUI>().enabled = true;
+
+        this.GetComponent<TextMeshProUGUI>()
+    .DOFade(1.0f,3.0f)
+    .Play();
+    }
+}
---
+++ TeamGifu/Assets/Assets/AssetsFile/Scripts/Gimic/Room5/MessgeKard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessgeKard : MonoBehaviour {
+    [SerializeField]
+   private FadaText fadeText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit");
+        if (other.gameObject.tag=="Hit")
+        {
+            Debug.Log("hit");
+            fadeText.fadeFunction();
+        }
+        
+    }
+}
